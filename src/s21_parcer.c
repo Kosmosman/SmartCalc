8554ebf0f970d820bc71de1eff4078ecb9c1493e
Joaquina Daeron <joaquind@va-a5.kzn.21-school.ru>
2022-12-19 14:10:19 +0300

    Выполнено преобразование в обратную польскую запись для арифметических операций

--- src/s21_parcer.c
+++ src/s21_parcer.c
@@ -2,7 +2,7 @@
 
 char* s21_parcing_string(stack** head, char* str) {
   int i = 0, count = 0, func_count = 0, res = 0;
-  int priority_str = 0, priority_stack = 0;
+  int priority = 0;
   // notation - хранит польскую запись с разделителями в виде '|'
   char* notation = (char*)calloc(512, sizeof(char));
   // func - буффер под функции
@@ -33,30 +33,27 @@ char* s21_parcing_string(stack** head, char* str) {
       // Если встречаем оператор - работает в зависимости от приоритетности и
       // заполненности стека
       if (strchr(OPERATORS, str[i]) && str[i] != '\0') {
-        if (*head != NULL) {
-          priority_str = (strchr(&OPERATORS[0], str[i]) - &OPERATORS[0]) / 3;
-          priority_stack =
-              (strchr(&OPERATORS[0], *(char*)(*head)->data) - &OPERATORS[0]) /
-              3;
-        }
-        if (*head == NULL || priority_str > priority_stack) {
-          func[0] = str[i++];
-          func[1] = '\0';
-          s21_push(head, func);
-          func[0] = '\0';
-        } else if (priority_str == priority_stack) {
-          notation[count++] = str[i++];
-          notation[count++] = SEPARATOR;
-        } else {
+        priority = s21_prioritization(str[i], *head);
+        func[0] = str[i];
+        func[1] = '\0';
+        while (priority) {
           count = s21_import_to_pn(head, notation, &pointer, count);
-          if (count >= 0) s21_push(head, &str[i++]);
+          priority = s21_prioritization(str[i], *head);
         }
+        i++;
+        if (count >= 0) s21_push(head, func);
+        func[0] = '\0';
+        // vizual_stack(*head);
       }
       // Если встречаем закрывающую скобку - переносим из стека в выходную
       // строку операторы до момента нахождения открыващей скобки
       if (str[i] == CLOSE_BRACKET) {
-        while (*(char*)(*head)->data != '(' && res != -1) {
+        while (*(char*)(*head)->data != '(' && count != -1) {
           count = s21_import_to_pn(head, notation, &pointer, count);
+        }
+        if (*(char*)(*head)->data != '(') {
+          res = s21_pop(head, &pointer);
+          if (res != -1) free(pointer);
         }
       }
     }
@@ -79,5 +76,33 @@ int s21_import_to_pn(stack** head, char* notation, void** pointer, int count) {
     free(*pointer);
   else
     count = -1;
+  // vizual_stack(*head);
   return count;
+}
+
+int s21_prioritization(char str, stack* head) {
+  int priority_str = 0, priority_stack = 0, res = 0;
+  if (head != NULL) {
+    priority_str = (strchr(&OPERATORS[0], str) - &OPERATORS[0]) / 3;
+    priority_stack =
+        (strchr(&OPERATORS[0], *(char*)(head)->data) - &OPERATORS[0]) / 3;
+    if (priority_str <= priority_stack) res = 1;
+  }
+  return res;
+}
+
+void randomize(void) {
+  int i = 0;
+  time_t t = 0;
+  char* str = "123456789";
+  char* str2 = "+-/*^";
+
+  /* Intializes random number generator */
+  srand((unsigned)time(&t));
+
+  /* Print 5 random numbers from 0 to 49 */
+  for (i = 0; i < 127; i++) {
+    printf("%c", str[rand() % 9]);
+    printf("%c", str2[rand() % 5]);
+  }
 }
